762b3b656160d6e01d0142b58334daa38dc3f76a
Valentin Lorentz <progval@progval.net>
2015-01-18 19:17:37 +0100

    Improve requesthandler test coverage.

--- tests/test_requesthandler.py
+++ tests/test_requesthandler.py
@@ -1,4 +1,4 @@
-from ppp_datamodel import Sentence
+from ppp_datamodel import Sentence, Resource
 from ppp_datamodel.communication import Request, TraceItem, Response
 from ppp_libmodule.tests import PPPTestCase
 from ppp_natural_math import app
@@ -7,6 +7,10 @@ class TestFollowing(PPPTestCase(app)):
     config_var = 'PPP_NATURALMATH'
     config = ''
     def testBasics(self):
+        q = Request('1', 'en', Resource('x'))
+        r = self.request(q)
+        self.assertEqual(r, [])
+
         q = Request('1', 'en', Sentence('integral of x^y'))
         r = self.request(q)
         self.assertEqual(len(r), 1, r)
